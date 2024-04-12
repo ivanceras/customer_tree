c8e66680cf59fdd10c469966352095de6b209eb5
Jovansonlee Cesar <ivanceras@gmail.com>
2024-04-13 03:18:56 +0800

    query for children customer and sponsored

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,16 @@ async fn main() -> anyhow::Result<()> {
     let ctx = Context::new();
 
     ctx.register_table("customer", data_source)?;
-    let data = ctx.sql("SELECT * FROM customer").await?;
-    println!("{}", data);
+    // top-level customer with no recruiter
+    let data = ctx.sql("SELECT * FROM customer WHERE parent_eq_id IS NULL").await?;
+    let d1 = ctx.sql("SELECT t1.eq_id, t1.full_name, 
+        (SELECT count(eq_id) as children FROM customer t3 WHERE t3.parent_eq_id = t1.eq_id)
+        FROM customer t1 ORDER BY children DESC LIMIT 20").await?;
+    d1.show()?;
+
+    let d2 = ctx.sql("SELECT t1.eq_id, t1.full_name, 
+        (SELECT count(eq_id) as sponsored FROM customer t3 WHERE t3.sponsor_eq_id = t1.eq_id)
+        FROM customer t1 ORDER BY sponsored DESC LIMIT 20").await?;
+    d2.show()?;
     Ok(())
 }
