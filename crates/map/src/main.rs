15bc1fdbc9231e4621e1b4a73d9c2649cd391965
Jovansonlee Cesar <ivanceras@gmail.com>
2025-06-01 04:09:56 +0800

    feat: add a code to extract city from a record value

--- crates/map/src/main.rs
+++ crates/map/src/main.rs
@@ -1,6 +1,7 @@
-use datafusion::prelude::*;
-//use datafusion::arrow::array::RecordBatch;
 use arrow::array::RecordBatch;
+use csv::ReaderBuilder;
+use csv::StringRecord;
+use datafusion::prelude::*;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,4 +20,19 @@ async fn main() -> anyhow::Result<()> {
     let pretty_results = arrow::util::pretty::pretty_format_batches(&results)?.to_string();
     println!("{}", pretty_results);
     Ok(())
+}
+
+#[test]
+fn test1() {
+    let addr = "Raadhuisstraat (2401231509), Amsterdam, 3036, NO";
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(addr.as_bytes());
+
+    let records: Vec<StringRecord> = rdr.records().map(|r| r.unwrap()).collect();
+    assert_eq!(records.len(), 1);
+    dbg!(&records);
+    let city = records[0].iter().rev().nth(2).unwrap().trim();
+    dbg!(&city);
+    panic!()
 }
